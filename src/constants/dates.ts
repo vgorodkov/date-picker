238d683e3587c7ef07fec92b0a351d8ea9bb25e5
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-23 13:28:48 +0300

    chore: add dates constants

---
+++ src/constants/dates.ts
@@ -0,0 +1,24 @@
+export const CURRENT_DAY = new Date().getDate();
+
+export const CURRENT_YEAR = new Date().getFullYear();
+
+export const CURRENT_MONTH = new Date().getMonth() + 1;
+
+export const WEEK_DAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+
+export const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'Jule',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const WEEKS_AMOUNT = 6;
