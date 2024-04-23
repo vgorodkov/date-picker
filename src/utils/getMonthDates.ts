da8219548d9b1a53a4772633f51ef61002fcfcbc
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-23 22:12:17 +0300

    feat: add possibility to start week from sunday or monday

--- src/utils/getMonthDates.ts
+++ src/utils/getMonthDates.ts
@@ -1,23 +1,24 @@
-import { MONTHS, WEEK_DAYS, WEEKS_AMOUNT } from '@/constants/dates';
+import { MONTHS, WEEK_DAYS_FROM_MONDAY, WEEKS_AMOUNT } from '@/constants/dates';
 import { MonthDate } from '@/types/date';
 
 import { getTimestampByDate } from './getTimestampByDate';
 
-export const getMonthDates = (year: number, month: number) => {
+export const getMonthDates = (year: number, month: number, firstDayOfWeek: 'monday' | 'sunday') => {
   const monthDates: MonthDate[] = [];
   const firstDayInMonth = new Date(year, Math.max(month - 1, 0), 1).getDay();
   const lastDayInMonth = new Date(year, month, 0).getDate();
   const lastDayInPrevMonth = new Date(year, Math.max(month - 1, 0), 0).getDate();
-  const restFromPrevMonth = firstDayInMonth === 0 ? WEEK_DAYS.length - 1 : firstDayInMonth - 1;
+  const restFromPrevMonth =
+    firstDayInMonth === 0 ? WEEK_DAYS_FROM_MONDAY.length - 1 : firstDayInMonth - 1;
 
-  for (let i = restFromPrevMonth - 1; i >= 0; i -= 1) {
+  for (let i = restFromPrevMonth - (firstDayOfWeek === 'monday' ? 1 : 0); i >= 0; i -= 1) {
     const date = {
       day: lastDayInPrevMonth - i,
       month: month === 1 ? MONTHS.length : month - 1,
       year: month === 1 ? year - 1 : year,
     };
 
-    monthDates.push({ ...date, timestamp: getTimestampByDate(date.year, date.month, date.day) });
+    monthDates.push({ ...date, timestamp: getTimestampByDate(date) });
   }
 
   for (let i = 1; i <= lastDayInMonth; i += 1) {
@@ -26,10 +27,10 @@ export const getMonthDates = (year: number, month: number) => {
       month,
       year,
     };
-    monthDates.push({ ...date, timestamp: getTimestampByDate(date.year, date.month, date.day) });
+    monthDates.push({ ...date, timestamp: getTimestampByDate(date) });
   }
 
-  const restToFillFromNextMonth = WEEKS_AMOUNT * WEEK_DAYS.length - monthDates.length;
+  const restToFillFromNextMonth = WEEKS_AMOUNT * WEEK_DAYS_FROM_MONDAY.length - monthDates.length;
 
   for (let i = 1; i <= restToFillFromNextMonth; i += 1) {
     const date = {
@@ -39,7 +40,7 @@ export const getMonthDates = (year: number, month: number) => {
     };
     monthDates.push({
       ...date,
-      timestamp: getTimestampByDate(date.year, date.month, date.day),
+      timestamp: getTimestampByDate(date),
     });
   }
   return monthDates;
