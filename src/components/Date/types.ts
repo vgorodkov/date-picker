c9913c60018e5b948671ab5796e9bfc8860da659
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-24 16:41:05 +0300

    refactor: rename component

--- src/components/Date/types.ts
+++
@@ -1,8 +0,0 @@
-import { DateVariant, MonthDate, Weekday } from '@/types/date';
-
-export interface DateProps {
-  date: MonthDate | Weekday;
-  variant?: DateVariant;
-  isSelected?: boolean;
-  selectDate?: (date: MonthDate) => void;
-}
