49dff1e94be35bc0737ac052cf551cf8acf4171a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-24 16:38:47 +0300

    chore: add functionality to select start of week

--- src/utils/getMonthDates.ts
+++ src/utils/getMonthDates.ts
@@ -1,17 +1,22 @@
-import { MONTHS, WEEK_DAYS_FROM_MONDAY, WEEKS_AMOUNT } from '@/constants/dates';
-import { MonthDate } from '@/types/date';
+import { MONTHS, WEEKS_AMOUNT } from '@/constants/dates';
+import { FirstWeekDay, MonthDate } from '@/types/date';
 
 import { getTimestampByDate } from './getTimestampByDate';
+import { getWeekDays } from './getWeekDays';
 
-export const getMonthDates = (year: number, month: number, firstDayOfWeek: 'monday' | 'sunday') => {
+export const getMonthDates = (year: number, month: number, firstDayOfWeek: FirstWeekDay) => {
+  const weekDays = getWeekDays(firstDayOfWeek);
   const monthDates: MonthDate[] = [];
   const firstDayInMonth = new Date(year, Math.max(month - 1, 0), 1).getDay();
   const lastDayInMonth = new Date(year, month, 0).getDate();
   const lastDayInPrevMonth = new Date(year, Math.max(month - 1, 0), 0).getDate();
-  const restFromPrevMonth =
-    firstDayInMonth === 0 ? WEEK_DAYS_FROM_MONDAY.length - 1 : firstDayInMonth - 1;
+  const restFromPrevMonth = firstDayInMonth === 0 ? weekDays.length - 1 : firstDayInMonth - 1;
 
-  for (let i = restFromPrevMonth - (firstDayOfWeek === 'monday' ? 1 : 0); i >= 0; i -= 1) {
+  for (
+    let i = restFromPrevMonth - (firstDayOfWeek === FirstWeekDay.MONDAY ? 1 : 0);
+    i >= 0;
+    i -= 1
+  ) {
     const date = {
       day: lastDayInPrevMonth - i,
       month: month === 1 ? MONTHS.length : month - 1,
@@ -30,7 +35,7 @@ export const getMonthDates = (year: number, month: number, firstDayOfWeek: 'mond
     monthDates.push({ ...date, timestamp: getTimestampByDate(date) });
   }
 
-  const restToFillFromNextMonth = WEEKS_AMOUNT * WEEK_DAYS_FROM_MONDAY.length - monthDates.length;
+  const restToFillFromNextMonth = WEEKS_AMOUNT * weekDays.length - monthDates.length;
 
   for (let i = 1; i <= restToFillFromNextMonth; i += 1) {
     const date = {
