7758b53fe8da0814695f37d393a2571cd865178c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-24 16:41:22 +0300

    refactor: remove unused styles and types

--- src/components/Datepicker/styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const CalendarContainer = styled.div`
-  position: relative;
-`;
--- src/components/Datepicker/types.ts
+++
@@ -1,3 +0,0 @@
-export interface DatepickerProps {
-  firstDayOfWeek: 'monday' | 'sunday';
-}
