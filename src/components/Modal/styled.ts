41241685463a1742e397b0303c55376988301ad7
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-25 14:53:11 +0300

    feat: add UI Modal component

---
+++ src/components/Modal/styled.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import { spacing } from '@/styles/spacing';
+
+export const ModalContainer = styled.div<{ $isActive: boolean }>`
+  height: 100vh;
+  width: 100vw;
+  background-color: #00000070;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  pointer-events: ${(props) => (props.$isActive ? 'all' : 'none')};
+  opacity: ${(props) => (props.$isActive ? 1 : 0)};
+  transition: 0.3s;
+`;
+
+export const ModalContent = styled.div<{ $isActive: boolean }>`
+  padding: ${spacing.m} ${spacing.l};
+  border-radius: 8px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: 0.3s;
+`;
