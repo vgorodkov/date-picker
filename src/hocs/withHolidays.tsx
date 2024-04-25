8fccd9890dae7945e8c0fa346fe32a0cbbfeaf99
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-25 14:55:41 +0300

    refactor: make withHoliday to accept holidays as argument

--- src/hocs/withHolidays.tsx
+++ src/hocs/withHolidays.tsx
@@ -1,14 +1,8 @@
 import { FC } from 'react';
 
 import { PickerProps } from '@/types/date';
-import { getTimestampByDate } from '@/utils/getTimestampByDate';
 
-export const withHolidays = (WrappedComponent: FC<PickerProps>) => {
-  const holidayTimestamps = [
-    getTimestampByDate({ day: 9, month: 5, year: 2024 }),
-    getTimestampByDate({ day: 1, month: 5, year: 2024 }),
-    getTimestampByDate({ day: 3, month: 5, year: 2024 }),
-  ];
+export const withHolidays = (WrappedComponent: FC<PickerProps>, holidayTimestamps: number[]) => {
   return ({ ...props }) => {
     return <WrappedComponent {...props} showHolidays holidayTimestamps={holidayTimestamps} />;
   };
