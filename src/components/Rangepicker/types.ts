da6b5dc86be6f8ca74c8a52f8179fd15f880a099
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-29 22:27:02 +0300

    refactor: remove unused reducer dut to custom hook usage

--- src/components/Rangepicker/types.ts
+++ src/components/Rangepicker/types.ts
@@ -8,20 +8,3 @@ export interface RangepickerState {
     endRangeDate: MonthDate | null;
   };
 }
-
-export type RangepickerAction =
-  | { type: 'OPEN_CALENDAR' }
-  | { type: 'CLOSE_CALENDAR' }
-  | { type: 'SELECT_DATE'; payload: MonthDate }
-  | { type: 'SET_SELECTED_INPUT'; payload: RangeVariant }
-  | { type: 'RESET_START_RANGE' }
-  | { type: 'RESET_END_RANGE' };
-
-export const initialState: RangepickerState = {
-  isCalendarOpen: false,
-  rangeSelection: {
-    selectedRangeInput: RangeVariant.START,
-    startRangeDate: null,
-    endRangeDate: null,
-  },
-};
