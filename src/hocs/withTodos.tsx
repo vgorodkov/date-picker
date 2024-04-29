73ca07920c0e374ec7cc2b8780a559f7e5247290
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-04-29 22:19:15 +0300

    refactor: move TodoModal to separate file

--- src/hocs/withTodos.tsx
+++ src/hocs/withTodos.tsx
@@ -1,56 +1,27 @@
 import { FC, useState } from 'react';
 
-import { Button } from '@/components/UI/Button';
-import { StyledInput } from '@/components/UI/Input/styled';
+import { TodoModal } from '@/components/TodoModal';
 import { Modal } from '@/components/UI/Modal';
-import { spacing } from '@/constants/spacing';
-import { FlexContainer } from '@/styles/common';
 import { MonthDate, PickerProps } from '@/types/date';
-import { zeroPad } from '@/utils/zeroPad';
 
 export const withTodos = (WrappedComponent: FC<PickerProps>) => {
-  return ({ ...props }) => {
-    const [todo, setTodo] = useState('');
+  return (props: PickerProps) => {
     const [selectedDate, setSelectedDate] = useState<MonthDate | null>(null);
     const [isModalActive, setIsModalActive] = useState(false);
-
-    const savedTodo = localStorage.getItem(
-      `${selectedDate?.day}-${selectedDate?.month}-${selectedDate?.year}`
-    );
-
-    // const todos = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
     const addTodo = (todoDate: MonthDate) => {
       setSelectedDate(todoDate);
       setIsModalActive(true);
     };
 
-    const saveTodo = () => {
+    const closeModal = () => {
       setIsModalActive(false);
-
-      localStorage.setItem(
-        `${selectedDate?.day}-${selectedDate?.month}-${selectedDate?.year}`,
-        todo
-      );
-      setTodo('');
     };
 
     return (
       <>
-        <Modal isActive={isModalActive} closeModal={() => setIsModalActive(false)}>
-          <FlexContainer $flexFlow="column nowrap" $alignItems="center" $gap={spacing.m}>
-            <h4>
-              {zeroPad(selectedDate?.day, 2)}/{zeroPad(selectedDate?.month, 2)}/{selectedDate?.year}
-            </h4>
-            <h5>Todo for this date:</h5>
-            <p>{savedTodo ?? 'No Todos yet'}</p>
-            <StyledInput
-              placeholder="Enter todo"
-              value={todo}
-              onChange={(e) => setTodo(e.target.value)}
-            />
-            <Button title="Save todo" onClick={saveTodo} />
-          </FlexContainer>
+        <Modal isActive={isModalActive} closeModal={closeModal}>
+          <TodoModal selectedDate={selectedDate} />
         </Modal>
         <WrappedComponent {...props} addTodo={addTodo} />
       </>
