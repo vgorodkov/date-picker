d42c401f70364c5f0cdd9121b376bfc309d9c88a
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-01 22:16:26 +0300

    chore: add casting to number for Date

--- src/utils/getTimestampByDate.ts
+++ src/utils/getTimestampByDate.ts
@@ -2,5 +2,5 @@ import { MonthDate } from '@/types/date';
 
 export const getTimestampByDate = (date: MonthDate) => {
   const { year, month, day } = date;
-  return new Date(year, month - 1, day).getTime();
+  return new Date(+year, +month - 1, +day).getTime();
 };
