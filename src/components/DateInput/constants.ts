d0c7b75b3a16e917baad6105dd8e790e1704cb16
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-02 12:46:47 +0300

    refactor: move constants to separate file

---
+++ src/components/DateInput/constants.ts
@@ -0,0 +1,9 @@
+export const cursorRanges = [
+  { start: 0, end: 2 },
+  { start: 3, end: 5 },
+  { start: 6, end: 10 },
+];
+
+export const BACKSPACE_KEY = 'Backspace';
+
+export const DIGITS_REGEXP = /^[0-9]/g;
