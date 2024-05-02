83092dd9163dc3804a68dafea1d88b083ebc804e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-02 11:32:36 +0300

    refactor: rename function to more logical name

---
+++ src/utils/isDateInRangeLimit.ts
@@ -0,0 +1,16 @@
+import { DateLimit, MonthDate } from '@/types/date';
+
+import { getTimestampByDate } from './getTimestampByDate';
+
+export const isDateInRangeLimit = (dateInput: MonthDate, dateLimit: DateLimit) => {
+  if (!dateInput) {
+    return false;
+  }
+
+  const dateTimestamp = getTimestampByDate(dateInput);
+  const minTimestamp = getTimestampByDate(dateLimit.min);
+  const maxTimestamp = getTimestampByDate(dateLimit.max);
+
+  const isInRange = dateTimestamp >= minTimestamp && dateTimestamp <= maxTimestamp;
+  return isInRange;
+};
