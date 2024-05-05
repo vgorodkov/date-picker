cc670d05967d6190f7f1004a3bead056a242c9bc
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-05 21:42:14 +0300

    refactor: change holidays to type without timestamp

--- src/hocs/withHolidays.tsx
+++ src/hocs/withHolidays.tsx
@@ -1,10 +1,10 @@
 import { FC } from 'react';
 
-import { MonthDate } from '@/types/date';
+import { CalendarDate } from '@/types/date';
 import { PickerProps } from '@/types/picker';
 import { getTimestampByDate } from '@/utils/getTimestampByDate';
 
-export const withHolidays = (WrappedComponent: FC<PickerProps>, holidayDates: MonthDate[]) => {
+export const withHolidays = (WrappedComponent: FC<PickerProps>, holidayDates: CalendarDate[]) => {
   const holidayTimestamps = holidayDates.map((holidayDate) => getTimestampByDate(holidayDate));
 
   return (props: PickerProps) => {
