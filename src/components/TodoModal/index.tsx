31662e1052610a178bb1a66c4f087a7e58fd5df0
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-05 11:52:25 +0300

    chore: move logic to custom hook; limit user input

--- src/components/TodoModal/index.tsx
+++ src/components/TodoModal/index.tsx
@@ -1,35 +1,52 @@
 import { ChangeEvent, useState } from 'react';
 
 import { Button, Input } from '@/components/UI';
+import { colors } from '@/constants/colors';
+import { DEFAULT_DAY_WITH_TIMESTAMP } from '@/constants/dates';
 import { spacing } from '@/constants/spacing';
+import { useTodosFromStorageByDate } from '@/hooks/useTodosFromStorageByDate';
 import { FlexContainer } from '@/styles/common';
 
-import { TodoModalHeader } from './TodoModalHeader';
+import { TODO_LENGTH_LIMIT } from './constants';
+import { TodoModalContainer } from './styled';
+import { TodoHeader } from './TodoHeader';
+import { TodoList } from './TodoList';
 import { TodoModalProps } from './types';
 
-export const TodoModal = ({ selectedDate }: TodoModalProps) => {
-  const [todo, setTodo] = useState('');
+export const TodoModal = ({ selectedDate = DEFAULT_DAY_WITH_TIMESTAMP }: TodoModalProps) => {
+  const [todoQuery, setTodoQuery] = useState<string>('');
 
-  const saveTodo = () => {
-    localStorage.setItem(`${selectedDate?.day}-${selectedDate?.month}-${selectedDate?.year}`, todo);
-    setTodo('');
-  };
+  const { todos, addTodo, clearTodos } = useTodosFromStorageByDate(selectedDate);
 
   const onTodoInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTodo(e.target.value);
+    const { value } = e.target;
+    setTodoQuery(value.slice(0, TODO_LENGTH_LIMIT));
+  };
+
+  const saveTodo = () => {
+    if (todoQuery.length > 0) {
+      addTodo(todoQuery);
+      setTodoQuery('');
+    }
   };
 
   return (
-    <FlexContainer $flexFlow="column nowrap" $gap={spacing.l}>
-      <TodoModalHeader selectedDate={selectedDate} />
-      <Input
-        label="Todo"
-        title="Add todo"
-        placeholder="Enter todo"
-        value={todo}
-        onChange={onTodoInputChange}
-      />
-      <Button title="Save todo" onClick={saveTodo} />
-    </FlexContainer>
+    <TodoModalContainer $flexFlow="column nowrap" $gap={spacing.l}>
+      <TodoHeader selectedDate={selectedDate} />
+      <TodoList todos={todos} />
+      <FlexContainer $flexFlow="column nowrap" $gap={spacing.l}>
+        <Input
+          label="Todo"
+          title="Add todo"
+          placeholder="Enter todo"
+          value={todoQuery}
+          onChange={onTodoInputChange}
+        />
+        <FlexContainer $gap={spacing.s}>
+          <Button color={colors.disabledHolidayText} title="Clear todos" onClick={clearTodos} />
+          <Button title="Save todo" onClick={saveTodo} />
+        </FlexContainer>
+      </FlexContainer>
+    </TodoModalContainer>
   );
 };
