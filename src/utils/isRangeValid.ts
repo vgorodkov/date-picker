3f5541ee1d29421d0ddea008690ee33a4f54102c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-05 12:02:28 +0300

    chore: make range invalid if start and end equal

--- src/utils/isRangeValid.ts
+++ src/utils/isRangeValid.ts
@@ -2,14 +2,14 @@ import { Range } from '@/types/range';
 
 export const isRangeValid = (range: Range) => {
   const { start, end } = range;
-  if (!start || !end) {
+
+  if (end <= start) {
     return false;
   }
-  if (end < start) {
+
+  if (start >= end) {
     return false;
   }
-  if (start > end) {
-    return false;
-  }
+
   return true;
 };
