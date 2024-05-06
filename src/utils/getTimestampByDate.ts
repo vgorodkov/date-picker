3c5ba4be2da1f07c41ee75972ebda503053b334c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-06 10:01:47 +0300

    fix: use proper type without timestamp

--- src/utils/getTimestampByDate.ts
+++ src/utils/getTimestampByDate.ts
@@ -1,8 +1,6 @@
-import { MonthDate } from '@/types/date';
+import { CalendarDate } from '@/types/date';
 
-interface NoTimestampMonthDate extends Omit<MonthDate, 'timestamp'> {}
-
-export const getTimestampByDate = (date: NoTimestampMonthDate) => {
+export const getTimestampByDate = (date: CalendarDate) => {
   const { year, month, day } = date;
   return new Date(+year, +month - 1, +day).getTime();
 };
