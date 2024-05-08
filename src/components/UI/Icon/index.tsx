a0565dc26eda4a2cac6397680aaded940e8d9a22
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-08 15:59:58 +0300

    refactor: remove redundant components

--- src/components/UI/Icon/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ImgHTMLAttributes } from 'react';
-
-import { StyledIcon } from './styled';
-
-interface IconProps extends ImgHTMLAttributes<HTMLImageElement> {}
-
-export const Icon: React.FC<IconProps> = ({ ...props }) => {
-  return <StyledIcon {...props} alt="icon" />;
-};
--- src/components/UI/Icon/styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledIcon = styled.img`
-  width: 16px;
-  height: 16px;
-  cursor: pointer;
-`;
