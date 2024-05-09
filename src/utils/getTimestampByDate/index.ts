5f9e621552268288fe4d184ed8d4b5f754a09bd8
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-05-09 20:17:38 +0300

    fix: remove redundant casting to number

--- src/utils/getTimestampByDate/index.ts
+++ src/utils/getTimestampByDate/index.ts
@@ -2,5 +2,5 @@ import { CalendarDate } from '@/types/date';
 
 export const getTimestampByDate = (date: CalendarDate) => {
   const { year, month, day } = date;
-  return new Date(+year, +month - 1, +day).getTime();
+  return new Date(year, month - 1, day).getTime();
 };
